22ad6b5310e686c7eb6f73375f14e1061e52f4d1
BuildTools <unconfigured@null.spigotmc.org>
2024-01-20 17:06:40 -0500

    Add new scripts and README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+You can obtain the scripts by typing ``git clone https://github.com/Xatra1/Random-Scripts``into a terminal.  
+  
+ip.js: Gets your IP using api.ipify.org, saves it to a file, reads the IP from the file and plays a success sound using ipsuccess.sh, and deletes the file.  
+Shell Scripts/ipsuccess.sh: The beep sound ip.js uses, plays using your motherboard's onboard speaker.  
+Shell Scripts/beep.sh: Plays a short jingle using your motherboard's onboard speaker.
--- ip.js
+++ ip.js
@@ -2,26 +2,30 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const url = 'https://api.ipify.org/?format=json';
 const method = { method: "Get" };
+const { exec } = require('promisify-child-process');
 fetch(url, method)
     .then(res => res.json())
     .then((json) => {
-        fs.writeFile('ip.json', JSON.stringify(json), function (e) {
-            if (e) {
-                return console.log(e);
-            }
+        fs.writeFile('ip.json', JSON.stringify(json), function (err) {
+            if (err) return console.log(err);
             console.log('File saved. Reading...')
             read();
         })
     })
 function read() {
-    fs.readFile('ip.json', function (e, data) {
-        if (e) {
-            return console.log(e);
-        }
+    fs.readFile('ip.json', function (err, data) {
+        if (err) return console.log(err);
         data = JSON.parse(data);
         console.log("Your IP is " + data.ip);
-        fs.unlink('ip.json', function () {
-            console.log("File removed.");
-        })
+        var beep = exec('sh "./Shell Scripts/ipsuccess.sh"',
+            (err, stderr) => {
+                if (err != null) {
+                    console.log("Error when playing success sound: " + stderr)
+                    console.log("If the error above states that 'beep' is not found, please run 'sudo apt install beep', reboot, and try again.");
+                }
+                fs.unlink('ip.json', function () {
+                    console.log("File removed.");
+                });
+            });
     })
 }
