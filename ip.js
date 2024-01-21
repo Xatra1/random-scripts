6087bdfcb982c397840dbb24ffb376b580e6607b
BuildTools <unconfigured@null.spigotmc.org>
2024-01-20 21:12:09 -0500

    Slight modifications

--- ip.js
+++ ip.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 const url = 'https://api.ipify.org/?format=json';
-const method = { method: "Get" };
+const method = { method: 'Get' };
 const { exec } = require('promisify-child-process');
 fetch(url, method)
     .then(res => res.json())
@@ -16,15 +16,15 @@ function read() {
     fs.readFile('ip.json', function (err, data) {
         if (err) return console.log(err);
         data = JSON.parse(data);
-        console.log("Your IP is " + data.ip);
-        var beep = exec('sh "./Shell Scripts/ipsuccess.sh"',
+        console.log('Your IP is ' + data.ip);
+        let beep = exec('sh "./Shell Scripts/ipsuccess.sh"',
             (err, stderr) => {
                 if (err != null) {
-                    console.log("Error when playing success sound: " + stderr)
-                    console.log("If the error above states that 'beep' is not found, please run 'sudo apt install beep', reboot, and try again.");
+                    console.log('Error when playing success sound: ' + stderr)
+                    console.log('If the error above states that "beep" is not found, please run "sudo apt install beep", reboot, and try again.');
                 }
                 fs.unlink('ip.json', function () {
-                    console.log("File removed.");
+                    console.log('File removed.');
                 });
             });
     })
