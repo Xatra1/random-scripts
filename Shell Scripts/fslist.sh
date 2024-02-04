8b50caa6a69c54ee4dfe170b2f9be20bf97ef164
Xatra1 <xatra169@gmail.com>
2024-02-03 23:01:41 -0500

    Greatly improve color output and make it optional

--- Shell Scripts/fslist.sh
+++ Shell Scripts/fslist.sh
@@ -8,13 +8,43 @@ echo -e ${warn}This script will log a huge amount of text to the console!${nc}
 echo Regular users do not have permission to read certain parts of the file system. This command needs to be run as root to fully list everything, which may require your password.
 read -p 'Do you want to run as root? (y/n/c): ' choice
 case $choice in
-[yY]*) echo 'This may take a while.' && echo Note: Not even root has permissions to access certain files. && sleep 2 && sudo dir -AR --color=yes / 2>&1 | tee fsout.log && echo Done! Output saved to fsout.log && echo Console will be cleared momentarily. ;;
-[nN]*) echo 'This may take a while.' && sleep 2 && dir --color=yes -AR / 2>&1 | tee fsout.log && echo Done! Output saved to fsout.log && echo Console will be cleared momentarily. ;;
-[cC]*) echo 'Cancelled. Console will be cleared momentarily.' ;;
+[yY]*)
+  read -p 'Do you want color output as well? Note: using color output will not create a log file. (y/n): ' choice2
+  case $choice2 in
+  [yY]*)
+    echo 'This may take a while.'
+    sleep 2
+    sudo dir --color=yes -AR /
+    echo Done!
+    ;;
+  [nN]*)
+    echo 'This may take a while.'
+    sleep 2
+    sudo dir --color=no -AR / 2>&1 | tee fsout.log
+    echo Done! Output saved to fsout.log
+    ;;
+  esac
+  ;;
+[nN]*)
+  read -p 'Do you want color output as well? Note: using color output will not create a log file. (y/n): ' choice2
+  case $choice2 in
+  [yY]*)
+    echo 'This may take a while.'
+    sleep 2
+    dir --color=yes -AR /
+    echo Done!
+    ;;
+  [nN]*)
+    echo 'This may take a while.'
+    sleep 2
+    dir --color=no -AR /
+    echo Done!
+    ;;
+  esac
+  ;;
+[cC]*) echo 'Cancelled.' ;;
 *) exit ;;
 esac
-sleep 2
-clear
 if [ -f ./fsout.log ]; then #Only use nano if script was not cancelled.
   nano fsout.log
 fi
