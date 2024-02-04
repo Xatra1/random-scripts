f212036782c84983726511eb21c286b62875c52d
Xatra1 <xatra169@gmail.com>
2024-02-03 23:13:12 -0500

    Update fslist.sh

--- Shell Scripts/fslist.sh
+++ Shell Scripts/fslist.sh
@@ -4,8 +4,8 @@ if [ -f ./fsout.log ]; then
 fi
 warn='\033[1;33m' #Change font color to yellow
 nc='\033[0m'      #Remove font color modifications
-echo -e ${warn}This script will log a huge amount of text to the console!${nc}
-echo Regular users do not have permission to read certain parts of the file system. This command needs to be run as root to fully list everything, which may require your password.
+echo -e ${warn}'This script will log a huge amount of text to the console!'${nc}
+echo 'Regular users do not have permission to read certain parts of the file system. This command needs to be run as root to fully list everything, which may require your password.'
 read -p 'Do you want to run as root? (y/n/c): ' choice
 case $choice in
 [yY]*)
