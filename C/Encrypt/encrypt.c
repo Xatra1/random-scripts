cbf5e87ead5708ce12bdc998213ca1486693ca45
Xatra1 <xatra169@gmail.com>
2024-02-16 23:38:35 -0500

    Upload encrypt.c

---
+++ C/Encrypt/encrypt.c
@@ -0,0 +1,104 @@
+//Header inclusion
+#include <stdio.h>
+#include <sys/stat.h> //For mkdir func
+#include <stdlib.h> //For exit func
+#include <unistd.h> //For access func
+
+void encrypt(FILE *Original, FILE *Encrypted, FILE *Key)
+{
+  int encryptionkey, i;
+  char realtextholder, encryptedtextholder;
+  Original = fopen("original.txt", "r");
+  Key = fopen("key.txt", "r");
+  if (Original != NULL)
+  {
+    if (Key != NULL)
+    {
+      fscanf(Key, "%d", &encryptionkey);
+      fseek(Original, 0L, SEEK_END);
+      int res = ftell(Original);
+      fclose(Original);
+      Original = fopen("original.txt", "r");
+      Encrypted = fopen("./results/cryptic.txt", "w");
+      for (i = 0; i < res; i++)
+      {
+        fread(&realtextholder, sizeof(char), 1, Original);
+        encryptedtextholder = realtextholder + encryptionkey;
+        fwrite(&encryptedtextholder, sizeof(char), 1, Encrypted);
+        printf("Encrypting Character %c ---> %c\n", realtextholder, encryptedtextholder);
+      }
+      fclose(Original);
+      fclose(Key);
+      fclose(Encrypted);
+    }
+    else
+    {
+      printf("You need to create a file named key.txt with the encryption key you wish to use, and place it in the same directory as this program.\n");
+    }
+  }
+  else
+  {
+    printf("You need to create a file named output.txt with the string you want to encrypt, and place it in the same directory as this program.\n");
+  }
+}
+
+void decrypt(FILE *Encrypted, FILE *NewOriginal, FILE *Key)
+{
+  int encryptionkey, i;
+  char passwordreader, realtextholder;
+  Encrypted = fopen("./results/cryptic.txt", "r");
+  Key = fopen("key.txt", "r");
+  if (Encrypted != NULL)
+  {
+    if (Key != NULL)
+    {
+      NewOriginal = fopen("./results/decrypted.txt", "w");
+      fscanf(Key, "%d", &encryptionkey);
+      fseek(Encrypted, 0L, SEEK_END);
+      int res = ftell(Encrypted);
+      fclose(Encrypted);
+      Encrypted = fopen("./results/cryptic.txt", "r");
+      for (i = 0; i < res; i++)
+      {
+        fread(&passwordreader, sizeof(char), 1, Encrypted);
+        realtextholder = passwordreader - encryptionkey;
+        fwrite(&realtextholder, sizeof(char), 1, NewOriginal);
+        printf("Decrypting Character %c ---> %c\n", passwordreader, realtextholder);
+      }
+      fclose(Encrypted);
+      fclose(NewOriginal);
+      fclose(Key);
+    }
+    else
+    {
+      printf("You are missing the required encryption key. Please make a file with the name 'key.txt' with the key you want to use for decryption, and palce it in the same directory as this program.\n");
+    }
+  }
+  else
+  {
+    printf("You do not have a file that needs decrypted. Please make a file named 'cryptic.txt' with the string you wish to decrypt, and place it in the 'results' directory.");
+  }
+}
+
+int main(FILE *Original, FILE *Encrypted, FILE *Key)
+{
+  FILE *MyOriginalFile, *MyEncryptedFile, *MyKeyFile, *MyNewOriginalFile;
+  char answer;
+  printf("Do you wish to encrypt or decrypt? (e/d/c):\n");
+  scanf("%c*", &answer);
+  switch (answer)
+  {
+  case 'e':
+    mkdir("results", 0700);
+    encrypt(MyOriginalFile, MyEncryptedFile, MyKeyFile);
+    break;
+  case 'd':
+    mkdir("results", 0700);
+    decrypt(MyEncryptedFile, MyNewOriginalFile, MyKeyFile);
+    break;
+  case 'c':
+    printf("Cancelled.\n");
+    break;
+  }
+  return 0;
+}
--- README.md
+++ README.md
@@ -3,8 +3,10 @@ You can obtain all files by cloning the repository.
 To run **ip.js**, you will first need to run ``npm install node-fetch fs promisify-child-process`` for the main script.  
 To run **encodestr.js**, you will first need to run ``npm install fs prompt-sync crypto``.  
 to run **encodeweb.js**, you will first need to run ``npm install fs http crypto``.  
-To compile **C/example.c** into an executable program, you will first need to run ``make && make clean`` in the same directory as ``example.c``.
+To compile **C/example.c** into an executable program, you will first need to run ``make && make clean`` in the same directory as ``example.c``.  
+To compile **C/encrypt.c** into an executable program, you will first need to run ``gcc -c encrypt.c -o encrypt`` in the same directory as ``encrypt.c``.  
 
+**These steps are not required if you are not using Ubuntu.**  
 For the beep scripts, you will need to perform the following in a terminal:  
 ``sudo apt install beep``  
 ``sudo nano /etc/modprobe.d/blacklist.conf``  
@@ -15,7 +17,7 @@ Save and exit by hitting Ctrl+S and Ctrl+X.
 Note: it is recommended to restore the original state of these lines once you're done using the scripts.  
 ``sudo usermod -aG input ${USER}``  
 ``reboot``  
-After the reboot is completed, the tones should play successfully.  
+After the reboot is completed, the tones should play successfully.    
   
 **ip.js:** Gets your IP using api.ipify.org, saves it to a file, reads the IP from the file, plays a success sound, and deletes the file. Execute with ``node ip.js``.  
 **encodestr.js:** Prompts the user to input a string and then encodes that string in Base64, while giving the option to write that string to a file, calculate the MD5 hash of the file, and encode that as well. Execute with ``node encodestr.js``.  
@@ -24,4 +26,5 @@ After the reboot is completed, the tones should play successfully.
 **shellscripts/beep.sh:** Plays a short jingle using your motherboard's onboard speaker. Execute with ``bash beep.sh``.  
 **shellscripts/beeplong.sh:** Same as beep.sh, with 4 more tones. Execute with ``bash beeplong.sh``.  
 **shellscripts/fslist.sh:** Lists every file and folder on your filesystem and outputs to fsout.log, with optional root access to allow for listing additional files. fsout.log will then be automatically opened in a text editor. Execute with ``bash fslist.sh``.  
-**C/example.c:**  Lists a bunch of information and outputs it to logs/log.txt, optionally outputting it to stdout as well. Can be executed by typing the filepath of the compiled executable into a terminal.  
+**C/example.c:** Lists a bunch of information and outputs it to logs/log.txt, optionally outputting it to stdout as well. Can be executed by typing the filepath of the compiled executable into a terminal.  
+**C/encrypt.c:** An encryption script that uses the key listed in 'key.txt' to encrypt the file 'original.txt', and can also decrypt 'results/cryptic.txt' using the same key. All outputs go into the newly created 'results' directory. Can be executed by typing the filepath of the compiled executable into a terminal. (This program will do nothing if executed using a different method)  
